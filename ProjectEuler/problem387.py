248c58c65347bc951f373a1f9f7454315b977be2
Alex Halfpenny <halfpeaw@hotmail.com>
2012-06-19 18:55:15 -0500

    Added Fast and Slow implementations

--- ProjectEuler/problem387.py
+++ ProjectEuler/problem387.py
@@ -20,10 +20,65 @@
 import sys
 sys.path.append("../SupportFunctions/")
 import EulerSupport
+import time
 
 getNum = lambda nums: sum(digit * 10 ** (len(nums) - 1 - i) for i, digit in enumerate(nums))
 
-#Return the true if it is a harshard as well as the number
+#
+#First Solution is on top, it was a bruteish force way of solving the problem when my fast
+#Approach wasn't working.  I did the simplie way first so I could confirm its results against 
+#The Fast way.
+
+
+#Determine if the number is Harshard Prime and all prev number
+def isPrimeHarshad(num):
+  s_num = str(num)
+  list_of_ints = [int(i) for i in s_num]
+  total = sum(list_of_ints)
+  if total == 0: return True
+  if (num % total == 0):
+    if EulerSupport.miller_rabin(int(num / total)):
+      #print("Test: " + str(num))
+      return recursiveHarshad(list_of_ints[:-1])
+  return False
+
+#Determine if a number and all previous our Harshard
+def recursiveHarshad(listInts):
+  #print(listInts)
+  if len(listInts) <= 1:
+    return True
+  val = getNum(listInts)
+  total = sum(listInts)
+  if val % total == 0:
+    return recursiveHarshad(listInts[:-1])
+  else:
+    #print("reject: " + str(val) + " total: " + str(total))
+    return False
+  
+
+#only works number n that our modulus 10
+def BruteForceSum(n):
+  print ("Brute Force For N: %d"%n)
+  print ("Start: " + time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
+  if n % 10 != 0:
+    print("Must end in a 0")
+    return(0)
+  n = int(n / 10)
+  total = 0
+  for x in range(1,n,1):
+      if (isPrimeHarshad(x)):
+        #print(x)
+        for c in "1234567890":
+          val = int(str(x)+c)
+          if (EulerSupport.miller_rabin(val)):
+          #if (True):
+            total+=val
+            #print(val)
+  print ("Total %d"%total)
+  print ("End: " + time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())+"\n")
+  return total
+  
+#Return True if the number is a Harshard
 def isHarshad(splitNum):
   total = sum(splitNum)
   if total == 0: return True
@@ -31,47 +86,72 @@ def isHarshad(splitNum):
   if (num % total != 0):
     return False
   return True
-#Currently Finds the sum of all harsah right deliminated numbers that end in prime.
-#Needs to find all that end in prime then R deliminate to Harshad, then R Deliminate to Prime
-def findSum(n):
+
+#Return True if the number is a Harshard Prime
+def isHarshadPrevPrime(splitNum):
+  total = sum(splitNum)
+  if total == 0: return True
+  num = getNum(splitNum)
+  if (num % total == 0):
+    if (EulerSupport.miller_rabin(int(num / total))):
+      return True
+  return False
+  
+def findSumFast(n):
+  print ("Fast Way For N: %d"%n)
+  print ("Start: " + time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
   total = 0
-  num = [0,0,0,0]
-  size = 1
-  i = 0
+  num = [0]*20
+  # We default to 100 because 0-99 are not valid answers
+  num[-3] = 1
+  #So we don't have to constantly computer length
   TOP = len(num)-1
-  while num[0] < 10:
-    if getNum(num) < 9:
-      num[TOP] = 0
-      i = TOP - 1
-      num[i] = 1
-    #cleanup step
+  i = TOP - 2
+  #while num[0] < 10:
+  #Constantly Converting the Array to an int is a 100% time increase.
+  #Could write code find the max array value and compares more constantly
+  #Or if you use powers of 10 exclusively like below
+  #while num[-15] < 10:
+  while getNum(num) < n:
+    #Clean Up step if we exceed the bounds of where we are set to 0 and go back up a spot
     if num[i] > 9:
       num[i] = 0
       i -=1
       num[i]+=1
-    elif i == TOP and EulerSupport.miller_rabin(getNum(num)):
-      val = getNum(num)
-      #print ("Found Prime: " + str(val))
-      #print("Total: " + str(total))
-      print(num)
-      total += val
-      num[TOP]+=1
-    # Or... Not of an Or
-    elif (not i == TOP and not isHarshad(num[:i+1])):
-      num[i]+=1
-    elif not i == TOP and isHarshad(num[:i+1]):
-      #if  getNum(num[:i]) == 0 or EulerSupport.miller_rabin(getNum(num[:i])):
-        i += 1
-      #else:
-      #  #print(num)
-      #  num[i] = 0
-      #  i-=1
-      #  num[i]+=1
+    #If our 1-Right deliminated value a Harshad and  Reduces to a prime
+    #Then go to the next highest value and check if prime
+    #Else no good and increment
+    elif i == TOP - 1:
+      if isHarshadPrevPrime(num[:i+1]):
+        i+=1
+      else:
+        num[i] += 1
+    #If we are at the top check to see if our total number is a prime, else increment
     elif i == TOP:
-      num[i]+=1
-  return total
+      if (EulerSupport.miller_rabin(getNum(num))):
+        val = getNum(num)
+        total += val
+        num[TOP]+=1
+      else:
+        num[i]+=1
+    #Every number preceding the TOP must be a Harshad, except 2nd to top thats Harshad Prime
+    elif i < TOP - 1:
+      if isHarshad(num[:i+1]):
+        i += 1
+      else:
+        num[i]+=1
+  print ("Total %d"%total)
+  print ("End: " + time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())+ "\n")
+  return total  
 if __name__ == "__main__":
   
   print ("Problem 387")
-  sum = findSum(100)
-  print (sum)
+  total = BruteForceSum(1000000)
+  total = findSumFast(1000000)
+  total = BruteForceSum(10000000)
+  total = findSumFast(10000000)
+  #10**14 = 696067597313468
+  total = findSumFast(10**14)
+  total = findSumFast(10**12+12)
+  total = findSumFast(10**13+13)
+  
