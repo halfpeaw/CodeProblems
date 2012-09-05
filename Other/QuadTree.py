2099da937d46ae5ff60c00932a5cba44a9803fa6
AWH - Laptop <halfpeaw@gmail.com>
2012-09-05 00:04:42 -0500

    Cleaned up Quadtree insert

--- Other/QuadTree.py
+++ Other/QuadTree.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append("../SupportFunctions/")
+import EulerSupport
+
 class Entry:
   def __init__(self, x, y, val):
     self.x = x
@@ -9,6 +13,10 @@ class Entry:
 class QNode:
   #bounds = [N,S,W,E]
   def __init__(self, limit, items, bounds, parent = None):
+    if limit < 1:
+      sys.exit("Limit needs to be atleast 1")
+    if items == None:
+      sys.exit("Bad initialization")
     self.items = items
     self.limit = limit
     self.NW = None
@@ -17,92 +25,81 @@ class QNode:
     self.SE = None
     self.parent = parent
     self.bounds = bounds
-    self.breakUpNode()
-  def breakUpNode(self): 
+    
+    self.CreateSubNodes()
+  def __repr__(self):
+    return "Node Bounds: %s"%self.bounds
+  #Debating if this would be better to just do this on the fly and load each item one at a time
+  def CreateSubNodes(self):
     if (self.items != None and len(self.items) > self.limit):
-      newItems = self.CreateSubLists(self.items)
+      if self.NW != None or self.NE != None or  self.SW != None or  self.SE != None:
+        sys.exit("Something is wrong in CreateSubNodes")
+      N = self.bounds["N"]
+      S = self.bounds["S"]
+      W = self.bounds["W"]
+      E = self.bounds["E"]
+      self.NW = QNode(self.limit, [], {"N":N, "S":(N+S)/2, "W":W, "E": (W+E)/2} , self)
+      self.NE = QNode(self.limit, [], {"N":N, "S":(N+S)/2, "W":(W+E)/2, "E":E} , self)
+      self.SW = QNode(self.limit, [], {"N":(N+S)/2, "S":S, "W":W, "E":(W+E)/2} , self)
+      self.SE = QNode(self.limit, [], {"N":(N+S)/2, "S":S, "W":(W+E)/2, "E":E}, self)
+      for entry in self.items:
+        if (self.within(self.NW.bounds, entry)):
+            self.NW.addItem(entry)
+        elif (self.within(self.NE.bounds, entry)):
+            self.NE.addItem(entry)
+        elif (self.within(self.SW.bounds, entry)):
+            self.SW.addItem(entry)
+        elif (self.within(self.SE.bounds, entry)):
+            self.SE.addItem(entry)
+        else:
+          print(self.bounds)
+          print("BAD! %s"%entry)
       self.items = None
-      self.NW = QNode(self.limit, newItems["NW"], 
-        newItems["NWBounds"], self)
-      self.SW = QNode(self.limit, newItems["SW"], 
-        newItems["SWBounds"], self)
-      self.NE = QNode(self.limit, newItems["NE"],
-        newItems["NEBounds"], self)
-      self.SE = QNode(self.limit, newItems["SE"], 
-        newItems["SEBounds"], self)
+    return
       
-  def addItem(self,entry):
-    if entry.x > self.bounds["E"] or entry.x < self.bounds["W"] or entry.y > self.bounds["N"] or entry.y < self.bounds["S"]:
-      return -1
-    #Really only need to check one you either have 4 children and are empty or no children and have the full list
-    node = self.determineQuandrant(entry)
-    #If Node is None that implies there are no children
-    if node != None:
-      node.addItem(entry)
-    elif self.items != None:
-      self.items.append(entry)
-      self.breakUpNode()
-    else:
-      print ("Something went wrong in AddItem")
-  
+  def within(self, bounds, entry):
+    if entry.x >= bounds["W"] and entry.x <= bounds["E"] and entry.y <= bounds["N"] and entry.y >= bounds["S"]:
+      return True
+    return False
+  #determine which node a entry would belong.  May make this recursive so it returns the actual node instead of the next level
   def determineQuandrant(self, entry):
-    x = entry.x
-    y = entry.y
     if (self.NW != None and self.NE != None and self.SW != None  and self.SE != None):
-      if (x < self.NW.bounds["E"] and x >= self.NW.bounds["W"] 
-            and y >= self.NW.bounds["S"] and y <= self.NW.bounds["N"]):
+      if (self.within(self.NW.bounds, entry)):
           return self.NW
-      elif (x >= self.NE.bounds["W"] and x <= self.NE.bounds["E"] 
-            and y >= self.NE.bounds["S"] and y <= self.NE.bounds["N"]):
+      elif (self.within(self.NE.bounds, entry)):
           return self.NE
-      elif (x < self.SW.bounds["E"] and x >= self.SW.bounds["W"] 
-            and y < self.SW.bounds["N"] and y >= self.SW.bounds["S"]):
+      elif (self.within(self.SW.bounds, entry)):
           return self.SW
-      elif (x >= self.SE.bounds["W"] and entry.x <= self.SE.bounds["E"] 
-            and y < self.SE.bounds["N"] and y >= self.SE.bounds["S"]):
+      elif (self.within(self.SE.bounds, entry)):
           return self.SE
       else:
         return None
     else:
-      return None
+      return self
+     
+  def addItem(self,entry):
+    if not self.within(self.bounds, entry):
+      print("Item not within bounds")
+      return -1
+    #Search through the nodes until you find a node that is accepting items aka items != None
+    node = self
+    while (node != None and node.items == None):
+      node = node.determineQuandrant(entry)
+    if node != None:
+      node.items.append(entry)
+      #Since we just added an item to our node we run createSubNodes.  
+      #If we have violated our node.limit it will break that node is to smaller parts
+      node.CreateSubNodes()
+    else:
+      print("Error in addItem")
+      sys.exit()
     
       
+  def determineIntersect(self, bounds):
+    None
     
-  #bounds = [N,S,W,E]
-  #Debating if this would be better to just do this on the fly and load each item one at a time
-  def CreateSubLists(self,items):
-    N = self.bounds["N"]
-    S = self.bounds["S"]
-    W = self.bounds["W"]
-    E = self.bounds["E"]
-    newItems = {}
-    newItems["NW"] = []
-    newItems["NE"] = []
-    newItems["SW"] = []
-    newItems["SE"] = []
-    newItems["BAD"] = []
-    newItems["NWBounds"] = {"N":N, "S":(N+S)/2, "W":W, "E": (W+E)/2} 
-    newItems["NEBounds"] = {"N":N, "S":(N+S)/2, "W":(W+E)/2, "E":E}  
-    newItems["SWBounds"] = {"N":(N+S)/2, "S":S, "W":W, "E":(W+E)/2}  
-    newItems["SEBounds"] = {"N":(N+S)/2, "S":S, "W":(W+E)/2, "E":E}  
-    for entry in items:
-      if (entry.x < newItems["NWBounds"]["E"] and entry.x >= newItems["NWBounds"]["W"] 
-            and entry.y >= newItems["NWBounds"]["S"] and entry.y <= newItems["NWBounds"]["N"]):
-          newItems["NW"].append(entry)
-      elif (entry.x >= newItems["NEBounds"]["W"] and entry.x <= newItems["NEBounds"]["E"] 
-            and entry.y >= newItems["NEBounds"]["S"] and entry.y <= newItems["NEBounds"]["N"]):
-          newItems["NE"].append(entry)
-      elif (entry.x < newItems["SWBounds"]["E"] and entry.x >= newItems["SWBounds"]["W"] 
-            and entry.y < newItems["SWBounds"]["N"] and entry.y >= newItems["SWBounds"]["S"]):
-          newItems["SW"].append(entry)
-      elif (entry.x >= newItems["SEBounds"]["W"] and entry.x <= newItems["SEBounds"]["E"] 
-            and entry.y < newItems["SEBounds"]["N"] and entry.y >= newItems["SEBounds"]["S"]):
-          newItems["SE"].append(entry)            
-      else:
-        newItems["BAD"].append(entry)
-        print(self.bounds)
-        print("BAD! %s"%entry)
-    return newItems
+
+  
   def printTree(self, indent = 0):
     space = " "*indent
     if (self.items != []):
@@ -120,9 +117,11 @@ class QNode:
   
 class QuadTree:
   #bounds = [N,S,W,E]
+  @EulerSupport.printTiming 
   def __init__(self, limit, items, bounds):
     print (limit)
     self.root = QNode(limit, items, bounds)
+  @EulerSupport.printTiming 
   def addEntry(self, entry):
     self.root.addItem(entry)
   def findEntry(self, entry):
@@ -141,15 +140,14 @@ class QuadTree:
 
 if __name__ == "__main__":
   print ("Quad Tree")
-  items = [Entry(0,0,1),Entry(0,2,2),Entry(3,0,1),Entry(-1,-1,1),Entry(1,0,1),
-          Entry(0,2.5,2),Entry(3.5,0,1),Entry(-.5,-1,6),Entry(-.7,0,4),Entry(0,2,8),]
+  items = [Entry(0,0,1),Entry(0,2,2),Entry(3,0,1),Entry(-1,-1,1),Entry(1,3,1),
+          Entry(0,2.5,2),Entry(3.5,0,1),Entry(-.5,-1,6),Entry(-.7,0,4),Entry(7,2,8),]
           #Entry(3,-2,1),Entry(-1,-1,3),Entry(0,0,5),Entry(0,2,2),Entry(3,0,1),Entry(-1,-1,1)]
   bounds = {"N":10.0, "S":-10.0, "W":-10.0,"E":10.0}
-  tree = QuadTree(5, items, bounds)
-  tree.printTree()
+  tree = QuadTree(2, items, bounds)
+  #tree.printTree()
   tree.addEntry(Entry(1,1,1000))
   tree.printTree()
-  
   
   
   
