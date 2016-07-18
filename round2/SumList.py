ade3bb25a114970e615d2ea9e1eacc4f86f71f2c
ahalf <ahalf@microsoft.com>
2016-07-17 23:10:57 -0700

    update to sumlist tests

--- round2/SumList.py
+++ round2/SumList.py
@@ -14,22 +14,57 @@ def listCount(list, target):
          first = first + 1
    print ("no solution found")
    return False
+   
+def noSpaceCount(list, target):
+   print("No Space restraints")
+   numbers = {}
+   for val in list:
+      if(val in numbers):
+         numbers[val] = numbers[val] + 1
+      else:
+         numbers[val]=1
+   for val in list:
+      temp = target - val
+      if (temp in numbers):
+         if (val == temp):
+            if(numbers[temp] > 1):
+               print("{} + {} = {}".format(val, temp, target))
+               return True
+         else:
+            print("{} + {} = {}".format(val, temp, target))
+            return True
+   print("No Solution Found")
+   return False
+         
+         
 
 
 if __name__ == "__main__":
-   list1 = [1,2,3,4]
-   target1 = 7
-   listCount(list1, target1)
+   #list1 = [1,2,3,4]
+   #target1 = 7
+   #listCount(list1, target1)
+   #
+   #list1 = [1,1,3,4]
+   #target1 = 8
+   #listCount(list1, target1)
+   #
+   #list1 = [1,-2,3,4]
+   #target1 = -1
+   #listCount(list1, target1)
+   #
+   #
+   #list1 = [1,-2,3,1]
+   #target1 = 2
+   #listCount(list1, target1)
    
-   list1 = [1,1,3,4]
-   target1 = 8
-   listCount(list1, target1)
+   list = [1,2,3,4]
+   target = 7
+   noSpaceCount(list, target)
    
-   list1 = [1,-2,3,4]
-   target1 = -1
-   listCount(list1, target1)
+   list = [1,1,2,7,4,2,5]
+   target = 4
+   noSpaceCount(list, target)
    
-   
-   list1 = [1,-2,3,1]
-   target1 = 2
-   listCount(list1, target1)
+   list = [0,1]
+   target = 0
+   noSpaceCount(list, target)
